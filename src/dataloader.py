fixed some normalization issues

src/dataloader.py
@@ -5,7 +5,7 @@ import torch
 import random
 from torch.utils.data import Dataset
 from decord import VideoReader, cpu
-from torchvision.transforms import Compose, Lambda
+from torchvision.transforms import Compose, Lambda, Normalize
 
 from decord import VideoReader, cpu
 
@@ -37,6 +37,6 @@ class VideoDataset(Dataset):
 # Define transformations
 transform = Compose([
     Lambda(lambda x: x.permute(0, 3, 1, 2).float()),
-    Lambda(lambda x: (x / 255.0) * 2 - 1)  # Normalize to [-1, 1]
+    Lambda(lambda x: (x / 255.0)),  # Normalize to [0, 1]
 ])
 

src/decoder/facedecoder.py
@@ -60,23 +60,4 @@ class FaceDecoder(nn.Module):
         apply_wd = apply_warping_field(g_1, w_d)
         g2 = self.g2d(apply_wd)
         return g2
-    
-    def save_model(self, path='./models/portrait/decoder/'):
-        """
-        Save the model parameters to the specified path.
-        
-        Args:
-        model (torch.nn.Module): The PyTorch model to save.
-        path (str): Path to the file where the model parameters are saved.
-        """
-        os.makedirs(path, exist_ok=True)
-        self.g3d.save_model(path + "g3d.pth")
-        self.g2d.save_model(path + "g2d.pth")
-        self.warping_module_d.save_model(path + "warping_module_d.pth")
-        self.warping_module_s.save_model(path + "warping_module_s.pth")
-    
-    def load_model(self, path='./models/portrait/decoder/'):
-        self.g3d.load_model(path + "g3d.pth")
-        self.g2d.load_model(path + "g2d.pth")
-        self.warping_module_d.load_model(path + "warping_module_d.pth")
-        self.warping_module_s.load_model(path + "warping_module_s.pth")
+

src/train/loss.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision.models import resnet18
 from src.train.discriminator import MultiScalePatchDiscriminator
+from torchvision.transforms import Normalize
 
 class PerceptualLoss(nn.Module):
     def __init__(self, config, arcface_model, gaze_model):
@@ -11,6 +12,7 @@ class PerceptualLoss(nn.Module):
         self.config = config
         self.arcface = arcface_model
         self.imageNet = resnet18(weights='IMAGENET1K_V1')
+        self.normalize = Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
         # freeze image net
         for param in self.imageNet.parameters():
@@ -31,8 +33,8 @@ class PerceptualLoss(nn.Module):
         Lface_scaled = Lface * self.arcface_weight
 
         # ImageNet ResNet-18 loss
-        pred_in = self.imageNet(pred)
-        target_in = self.imageNet(driver)
+        pred_in = self.imageNet(self.normalize(pred))
+        target_in = self.imageNet(self.normalize(driver))
         Lin = F.l1_loss(pred_in, target_in)  # Normalize over batch
         Lin_scaled = Lin * self.imagenet_weight
 